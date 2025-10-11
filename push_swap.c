9ce910d57cd08277e50307827b0e15b7a3e7ffb4
gabrgarc <gabrgarc@student.42sp.org.br>
2025-10-11 14:43:55 -0300

    small refactoring

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:57:11 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/09 20:39:23 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/11 14:09:43 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int	main(int argc, char **argv)
 {
+	t_lst	*list;
 	char	*str;
 
 	if (argc < 2)
-		return (0);
-	if (argc > 2)
-	{
-		valid_inputs(++argv);
-		str = ft_rsplit(args, ' ');
-	}
-	if (!str)
-		str = argv[1];
-	create_stack(str);
+		return (0);//function than print "Error" and finish program
+	str = NULL;
+	valid_args(&argv[1], &str);
+	list = create_list(str);
+	free(str);
+	valid_numbers(&list);
+	valid_list(&list);
+	lstclear(&list, free);
 	write(1, "Ok\n", 3);
 	return (1);
 }
