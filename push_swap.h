03e460abf753e3d78cf8bda8c1f85d8c2e778068
GabrielGem <gabrielgarciamendes91@gmail.com>
2025-10-13 10:41:55 -0300

    update: new functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:57:33 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/11 14:10:08 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/12 12:33:31 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,32 @@ typedef struct	s_lst
 	struct s_lst	*next;
 }	t_lst;
 
+enum	e_options
+{
+	CLEAR,
+	CLEARSTR,
+	CLEARLIST,
+	CLEARSTACK,
+	ERROR,
+	ERRORSTR,
+	ERRORLIST,
+	ERRORSTACK
+};
+
 int	main(int argc, char **argv);
 
 // validations
-void	valid_args(char **args, char **str);
+t_lst	*validations(char **args);
 void	valid_str(char *str);
-void	valid_numbers(t_lst **list);
-void	valid_list(t_lst **list);
+void	valid_double_numbers(t_lst **list);
+void	valid_order(t_lst **list);
+// list
 t_lst	*create_list(char *str);
 t_lst	*new_node(int content);
 void	addnode_back(t_lst **lst, t_lst *new);
 void	lstclear(t_lst **lst, void (*del)(void *));
+// clear and messages
+void	memclear(void *adress, int option);
+void	error_message(void);
 
 #endif
