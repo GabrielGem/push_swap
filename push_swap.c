7e69dc47a9ba001f3f4b561b1df9a5dd30b2ffa9
GabrielGem <gabrielgarciamendes91@gmail.com>
2025-10-13 10:53:31 -0300

    pass responsibity of validations for one function

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:57:11 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/11 14:09:43 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/12 12:40:59 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,9 @@
 int	main(int argc, char **argv)
 {
 	t_lst	*list;
-	char	*str;
 
 	if (argc < 2)
-		return (0);//function than print "Error" and finish program
-	str = NULL;
-	valid_args(&argv[1], &str);
-	list = create_list(str);
-	free(str);
-	valid_numbers(&list);
-	valid_list(&list);
-	lstclear(&list, free);
-	write(1, "Ok\n", 3);
+		return (0);
+	list = validations(&argv[1]);
 	return (1);
 }
