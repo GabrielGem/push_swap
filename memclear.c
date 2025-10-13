642023e5f30216a483e64144ed6b9d79261fc7b8
GabrielGem <gabrielgarciamendes91@gmail.com>
2025-10-13 10:54:36 -0300

    add: function for clear and exit or just clear

---
+++ memclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memclear.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 11:26:58 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/10/13 10:30:13 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	memclear(void *adress, int option)
+{
+	if (option == CLEARSTR || option == ERRORSTR)
+		free(adress);
+	if (option == CLEARLIST || option == ERRORLIST)
+		lstclear(&adress, free);
+	if (option == ERRORSTR || option == ERRORLIST)
+		error_message();
+}
+
+void	error_message(void)
+{
+	write(1, "Error\n", 6);
+	exit (0);
+}
