36a64e04b899a27b499d6576cfadd46aad6d2a42
GabrielGem <gabrielgarciamendes91@gmail.com>
2025-10-20 13:15:14 -0300

    test moviments

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:57:11 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/10/12 12:40:59 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/10/14 12:02:20 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 int	main(int argc, char **argv)
 {
 	t_lst	*list;
+	t_lst	*listb;
 
 	if (argc < 2)
 		return (0);
 	list = validations(&argv[1]);
+	listb = NULL;
+	print_list(&list, &listb);
+	moviments(&list, &listb);
+	print_list(&list, &listb);
+	lstclear(&list, free);
 	return (1);
 }
