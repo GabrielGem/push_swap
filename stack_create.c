95a7782c32c702ad4547f63b05d0091c3a93e99d
gabrgarc <gabrgarc@student.42sp.org.br>
2025-11-16 13:08:00 -0300

    rename file and function

--- stack_create.c
+++ stack_create.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stacks.c                                           :+:      :+:    :+:   */
+/*   create_stack.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 15:26:46 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/11/08 20:10:33 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/11/16 12:17:08 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*stack_list(char *str)
+t_stack	*stack_create(char *str)
 {
 	t_stack	*list;
 	t_stack	*temp;
@@ -29,23 +29,13 @@ t_stack	*stack_list(char *str)
 			break ;
 		value = ft_atol(&str[i]);
 		if (value > MAX_INT || value < MIN_INT)
-			memclear(str, CLEARSTR);
+			free(str);
 		if (value > MAX_INT || value < MIN_INT)
 			memclear(list, ERRORLIST);
-		temp = (t_stack *)new_node(value, NULL, VALUE);
+		temp = stack_new_node(value);
 		while (str[i] && (ft_isdigit(str[i]) || ft_issignal(str[i])))
 			i++;
 		stack_add_back(&list, temp);
 	}
 	return (list);
-}
-
-void	move_list(t_orch *info, char *move)
-{
-	t_moves	*temp;
-
-	if (!move)
-		return ;
-	temp = (t_moves *)new_node(0, move, MOVE);
-	move_add_back(&info->moves, temp);
 }
